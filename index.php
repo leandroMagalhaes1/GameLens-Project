bb34b5ac73a9a36700c342af7f58d4cafe83f32a
Leandro Marins Magalhães <146432380+leandroMagalhaes1@users.noreply.github.com>
2024-10-24 21:33:38 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GameLens</title>
+    <link rel="stylesheet"href="style_index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"/>
+</head>
+<body>
+    <nav class="index_navbar">
+        <div class="index_navdiv">
+            <ul class="index_nav_list_logo">
+                <li><div class="index_nav_logo"><a href="index.php"><img src="imagens/LogoGameLensNav.png"></a></div></li>
+                <li><a class="index_nav_list_elements" href="index.php"><span class="span_logo">GameLens</span></a></li>
+            </ul>
+            
+            <ul class="index_nav_list">
+                <li><a class="index_nav_list_elements" id="index_nav_list_element_yourlist" href="#">Your List <i class="fa-solid fa-scroll"></i></a></li>
+            </ul>
+        
+        </div>
+    </nav>
+
+    <div class="index_hero">
+        <img id="index_hero_image" src="imagens/heroImg.png">
+        <div class="index_container">
+            <header>Explore Others Lists</header>
+            <p>Discover what other people think about all types of games</p>
+            <button class="index_explore">
+                Explore <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+        </div>
+        
+    </div>
+
+    
+    
+</body>
+</html>
