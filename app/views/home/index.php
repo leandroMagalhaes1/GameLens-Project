92a1b4d612d37e5d97a5d15eb35c0498a6a54f5b
leandroMagalhaes1 <146432380+leandroMagalhaes1@users.noreply.github.com>
2024-10-27 23:46:58 +0000

    routing is kinda working

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GameLens</title>
+    
+    <link rel="stylesheet" href="public/css/style_index.css">
     <link rel="stylesheet" href="css/style_index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"/>
 </head>
@@ -11,7 +13,7 @@
     <nav class="index_navbar">
         <div class="index_navdiv">
             <ul class="index_nav_list_logo">
-                <li><div class="index_nav_logo"><a href="index.php"><img src="images/LogoGameLensNav.png"></a></div></li>
+                <li><div class="index_nav_logo"><a href="/app/views/home/index.php"><img src="images/LogoGameLensNav.png"></a></div></li>
                 <li><a class="index_nav_list_elements" href="index.php"><span class="span_logo">GameLens</span></a></li>
             </ul>
             <div class="index_nav_list">
